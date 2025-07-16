7ee5a4508a74f98214750e207fd260e0f0d44381
Irving Cruz Chávez <ic3386941@gmail.com>
2025-07-16 17:53:14 -0600

    [FIX]

---
+++ src/components/Prueba.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Prueba = () => {
+    const estilo = {
+        minWidth: '320px',
+        height: '700px'
+    }
+  return (
+    <div>
+      <div
+        className="calendly-inline-widget"
+        data-url="https://calendly.com/ic3386941/reunion-con-el-cliente"
+        style={ estilo }
+      ></div>
+      <script
+        type="text/javascript"
+        src="https://assets.calendly.com/assets/external/widget.js"
+        async
+      ></script>
+    </div>
+  );
+};
