683a5d856f8e04a1d0f891837c1962572f934ff6
Kevin <kevinbadillod@gmail.com>
2025-07-17 15:35:38 -0600

    [ADD] Adicion de la barra hamburguesa al componete Navbar en resoluciones de 480px

--- src/components/Fomulary.jsx
+++ src/components/Fomulary.jsx
@@ -1,106 +1,68 @@
-import React, { useState } from "react";
-import "./Formulary.css";
+import React, { useEffect } from "react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
 
 const Formulary = () => {
-  const [nombre, setNombre] = useState("");
-  const [apellidos, setApellidos] = useState("");
-  const [telefono, setTelefono] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!nombre || !apellidos || !telefono) {
-      alert("Por favor completa todos los campos");
-      return;
-    }
-
-    console.log({ nombre, apellidos, telefono });
-    alert(`Formulario enviado: ${nombre} ${apellidos} - ${telefono}`);
-
-     try {
-          const res = await fetch("http://localhost:3000//enviarformulario", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ nombre, apellidos, telefono }),
-          });
-          setEmail("");
-          if (res.ok) {
-            Swal.fire({
-              title: "Te has suscrito con éxito",
-              text: "Te hemos enviado un correo. Puedes revisarlo en Gmail.",
-              icon: "success",
-              showCancelButton: true,
-              confirmButtonText: "Ir a Gmail",
-              cancelButtonText: "Cerrar",
-              showClass: {
-                popup: `
-          animate__animated
-          animate__fadeInUp
-          animate__faster
-        `,
-              },
-              hideClass: {
-                popup: `
-          animate__animated
-          animate__fadeOutDown
-          animate__faster
-        `,
-              },
-            }).then((result) => {
-              if (result.isConfirmed) {
-                window.open("https://mail.google.com", "_blank");
-              }
-            });
-          } else {
-            Swal.fire({
-              icon: "error",
-              title: "Oops...",
-              text: "Algo salio mal!",
-            });
-          }
-        } catch (error) {
-          console.error("Error al enviar el formulario", error);
-          setMensaje("Error en el servidor.");
-        }
+  const estilo = {
+    minWidth: "320px",
+    height: "700px",
   };
 
+  //* Script de calendly inicio
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+  //* Script de calendly Fin
+  //? Escuchar el evento de calendly ---- Inicio
+  useEffect(() => {
+    const CalendlyEvent = async (e) => {
+      if (e.data.event === "calendly.event_scheduled") {
+
+        await MySwal.fire({
+          title: "¡Cita agendada con éxito!",
+          text: "Ahora serás redirigido a Gmail.",
+          icon: "success",
+          confirmButtonText: "Ir a Gmail",
+        });
+
+        window.open("https://mail.google.com", "_self");
+
+        try {
+          await fetch("http://localhost:3000/enviarformulario", {
+            method: "POST",
+          });
+          console.log(" POST enviado");
+        } catch (error) {
+          console.error("Error enviando: ", error);
+        }
+      }
+    };
+
+    window.addEventListener("message", CalendlyEvent);
+    return () => {
+      window.removeEventListener("message", CalendlyEvent);
+    };
+  }, []);
+
+  //? Escuchar el evento de calendly ---- Fin
+
   return (
-    <form className="formulario__container__abc123" onSubmit={handleSubmit}>
-      <h2 className="formulario__titulo__abc123">Formulario</h2>
-
-      <input
-        className="formulario__input__abc123"
-        type="text"
-        name="Nombre"
-        placeholder="Nombre"
-        value={nombre}
-        onChange={(e) => setNombre(e.target.value)}
-      />
-
-      <input
-        className="formulario__input__abc123"
-        type="text"
-        name="Apellidos"
-        placeholder="Apellidos"
-        value={apellidos}
-        onChange={(e) => setApellidos(e.target.value)}
-      />
-
-      <input
-        className="formulario__input__abc123"
-        type="tel"
-        name="Telefono"
-        placeholder="Número de teléfono"
-        value={telefono}
-        onChange={(e) => setTelefono(e.target.value)}
-      />
-
-      <button className="formulario__boton__abc123" type="submit">
-        Enviar
-      </button>
-    </form>
+    <div>
+      <div
+        className="calendly-inline-widget"
+        data-url="https://calendly.com/ic3386941/reunion-con-el-cliente"
+        style={estilo}
+      ></div>
+    </div>
   );
 };
 
